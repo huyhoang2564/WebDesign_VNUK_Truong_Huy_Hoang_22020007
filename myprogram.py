534a4d8d46b86d0da2e35ad477f80dc18c3b7c76
huyhoang2564 <huyhoang2564@gmail.com>
2024-03-15 08:07:53 +0700

    Huy Hoang gui bai code python

---
+++ myprogram.py
@@ -0,0 +1,11 @@
+'''
+---------------------------------
+Problem 1 - MY PROGRAM
+TRUONG HUY HOANG
+22.06.2022
+---------------------------------
+'''
+
+import tinhtoan
+tinhtoan.area(3,5)
+tinhtoan.perimeter(3,5)
